14515cb12d2e248751c8949b8040b0d9b032da34
jason gao <jas_gao@msn.com>
2013-01-03 23:22:40 +0000

    Added Project for Chapter 4 Domain Model

---
+++ ASPPattern/ASPPatterns.Chap3.Layered.Model/Price.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ASPPatterns.Chap3.Layered.Model
+{
+    public class Price
+    {
+        private IDiscountStrategy _discountStrategy = new NullDiscountStrategy();
+        private decimal _rrp;
+        private decimal _sellingPrice;
+
+
+        public Price(decimal rrp, decimal sellingPrice)
+        {
+            _rrp = rrp;
+            _sellingPrice = sellingPrice;
+        }
+
+        public void SetDiscountStrategyTo(IDiscountStrategy discountStrategy)
+        {
+            this._discountStrategy = discountStrategy;
+        }
+
+        public decimal SellingPrice
+        {
+            get { return _discountStrategy.ApplyExtraDiscountTo(_sellingPrice); }
+        }
+
+        public  decimal RRP
+        {
+            get { return _rrp; }
+        }
+
+        public decimal Discount
+        {
+            get { return RRP > SellingPrice ? RRP - SellingPrice : 0; }
+        }
+        
+        public decimal Savings
+        {
+            get { return RRP > SellingPrice ? 1 - (SellingPrice/RRP) : 0; }
+        }
+    }
+}
---
+++ ASPPattern/ASPPatterns.Chap3.Layered.Model/ProductService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ASPPatterns.Chap3.Layered.Model
+{
+    public class ProductService
+    {
+        private IProductRepository _productRepository;
+
+        public ProductService(IProductRepository discountStrategy)
+        {
+            this._productRepository = discountStrategy;
+        }
+
+        public IList<Product> GetAllProductFor(CustomerType customerType)
+        {
+            IDiscountStrategy discountStrategy = DiscountFactory.GetDiscountStrtegyFor(customerType);
+            IList<Product> products = _productRepository.Findall(); 
+            products.Apply(discountStrategy);
+            return products;
+
+        }
+
+
+    }
+}
