4ec4c9246488700af765382a7b4259857a0040b0
jason gao <jas_gao@msn.com>
2013-01-03 23:24:51 +0000

    Added Project for Chapter 4 Domain Model

---
+++ ASPPattern/ASPPatterns.Chap3.Layered.Service/ProductListResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ASPPatterns.Chap3.Layered.Service
+{
+    public class ProductListResponse
+    {
+        public bool Success { get; set; }
+        public String Message { get; set; }
+        public IList<ProductViewModel> Products { get; set; }
+        
+
+    }
+}
