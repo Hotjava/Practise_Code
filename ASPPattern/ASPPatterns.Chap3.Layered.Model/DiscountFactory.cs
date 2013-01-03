b34b39c72d9d581ad2ec84a9e4897b109a490270
jason gao <jas_gao@msn.com>
2013-01-03 23:20:53 +0000

    Added Project for Chapter 4 Domain Model

---
+++ ASPPattern/ASPPatterns.Chap3.Layered.Model/DiscountFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ASPPatterns.Chap3.Layered.Model
+{
+    public static class DiscountFactory
+    {
+        public static IDiscountStrategy GetDiscountStrtegyFor(CustomerType type)
+        {
+            switch (type)
+            {
+                case (CustomerType.Trade):
+                    {
+                        return new TradeDiscountStrategy();
+                    }
+
+                default:
+                    {
+                        return new NullDiscountStrategy();
+                    }
+                
+            }
+        }
+
+    }
+}
